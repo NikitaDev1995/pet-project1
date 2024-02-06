fad53d0bc4742763d7eeacff631edada80b38f8f
Nikita Skripka <>
2024-02-06 19:21:28 +1000

    Создание и настройка ячейки и распределение проекта по файлам.

---
+++ TrainEnglishApp/TrainEnglishApp/Controller/DictionaryTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DictionaryTableViewController.swift
+//  TrainEnglishApp
+//
+//  Created by Nikita Skripka on 06.02.2024.
+//
+
+import UIKit
+
+class DictionaryTableViewController: UITableViewController {
+
+    //MARK: - Life scene cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let nib = UINib(nibName: "DictionaryTableViewCell", bundle: nil)
+        tableView.register(nib, forCellReuseIdentifier: "DictionaryCell")
+        tableView.estimatedRowHeight = 44
+        tableView.rowHeight = UITableView.automaticDimension
+    }
+}
+
+//MARK: - Extension DictionaryTableViewController (UITableVIewDelegate, UITableViewDataSource)
+extension DictionaryTableViewController {
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DictionaryCell", for: indexPath)
+        return cell
+    }
+}
