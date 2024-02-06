b982ec6f9137fd6380ed6027731ff2dbe819591a
Nikita Skripka <>
2024-02-06 18:02:05 +1000

    Добавление проекта в репозиторий

---
+++ TrainEnglishApp/TrainEnglishApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainEnglishApp
+//
+//  Created by Nikita Skripka on 06.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
